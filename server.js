ebe53625c810145a6ff3fa5459f322f3a0f4cb61
Nihit Verma <122816401+Nihitv101@users.noreply.github.com>
2023-07-01 23:36:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/', (req, res)=>{
 
 
 // deployment config
-__dirname = path.resolve();
+const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
